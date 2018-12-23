cd5ca1ae9d339260792544566e66a91ff2302d83
Kevin Rogbone <contact@kevin.house>
2018-12-22 22:51:40 -0500

    Delete Halcyon
    Test didn't work

--- halcyon/login/logout.php
+++
@@ -1,3 +0,0 @@
-<script src="/assets/js/jquery/jquery.min.js"></script>
-<script src="/assets/js/jquery-cookie/src/jquery.cookie.js"></script>
-<script>localStorage.clear();$.removeCookie('session');location.href="/login";</script>
--- halcyon/widgets/overlay_copy_link.php
+++
@@ -1,8 +0,0 @@
-<div class="overlay_simple overlay_copy_link invisible">
-<header class="overlay_simple_header">
-<span class="emoji_poss"><?=_('Copy link to Toot')?></span>
-</header>
-<div class="overlay_simple_body">
-<input class="copy_link_input" type="text" value="" readonly>
-</div>
-</div>
